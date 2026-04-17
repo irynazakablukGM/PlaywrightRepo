ef5c5942896bdfd0a4db20914c420d721ed93861
“irynazakablukGM” <irynazakabluk@gmail.com>
2026-04-17 17:57:50 +0300

    Added skip for login test

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Login with valid credentials', async ({ page }) => {
+    test.skip(!!process.env.CI, 'Skip login test on CI env');
   await page.goto('https://practicesoftwaretesting.com/auth/login');
   await page.getByLabel('Email address').fill('customer@practicesoftwaretesting.com');
   await page.getByTestId('password').fill('welcome01');
