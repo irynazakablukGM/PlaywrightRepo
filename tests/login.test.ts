acba8862adf1f1ad290177374b1b86e69641a401
“irynazakablukGM” <irynazakabluk@gmail.com>
2026-04-20 10:31:35 +0300

    Added required fixes for baseURL and selectors

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -2,11 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('Login with valid credentials', async ({ page }) => {
     test.skip(!!process.env.CI, 'Skip login test on CI env');
-  await page.goto('https://practicesoftwaretesting.com/auth/login');
-  await page.getByLabel('Email address').fill('customer@practicesoftwaretesting.com');
+  await page.goto('/auth/login');
+  await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
   await page.getByTestId('password').fill('welcome01');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+  await page.getByTestId('login-submit').click();
+  await expect(page).toHaveURL('/account');
   await expect(page.getByTestId('page-title')).toContainText('My account');
   await expect(page.getByTestId('nav-menu')).toContainText('Jane Doe');
 }
